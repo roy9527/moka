11cd554c59f7f65c6b2278dc837a4880989d952d
kin_w <myroy7@163.com>
2018-03-27 11:54:56 +0800

    update templates

--- hello.py
+++ hello.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
