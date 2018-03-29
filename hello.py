5568a814b50e463a454b03d4b3f95754a7c38ca8
kin <zhangjing@iappease.com.cn>
2018-03-29 18:59:34 +0800

    update

--- hello.py
+++ hello.py
@@ -1,20 +1,53 @@
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask import Flask, render_template
+from datetime import datetime
+import sys, os
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
 
+baseDir = os.path.abspath(os.path.dirname(__file__))
+
+print(baseDir)
+
+class NameForm(FlaskForm):
+    name=StringField('What is your name?', validators=[Required()])
+    submit=SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='its your dream.'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(baseDir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name=None
+    form=NameForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data=''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
 if __name__ =='__main__':
-    # app.run(debug=True)
     manager.run()
